b95cd8544956c68fb59c3ecd8b9509f8cd822561
itoledo <itoledo@MODULA02-VIRTUAL>
2014-05-18 12:26:15 +0200

    Modificacion Rama Test

--- HolaED/src/HolaED.java
+++ HolaED/src/HolaED.java
@@ -3,7 +3,7 @@ public class HolaED {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("RAMA TEST");
+		System.out.println("MODIFICACION RAMA TEST");
 
 	}
 
